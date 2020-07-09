4e8fe40c6a47fbf4ee5e1712981b0fb34e050714
ceberm <c.bg@live.com>
2020-07-08 23:26:28 -0600

    Opening settings Intent

--- app/src/main/java/com/example/getsomesettings/MainActivity.java
+++ app/src/main/java/com/example/getsomesettings/MainActivity.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "Fibonacci::";
+    private static final String TAG = "GetSomeSettings::";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +26,64 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView text = findViewById(R.id.txt_process);
-        ActivityManager am = (ActivityManager) getApplicationContext().
-                getSystemService(Activity.ACTIVITY_SERVICE);
-        RunningTaskInfo foregroundTaskInfo = am.getRunningTasks(10).get(0);
-        packageName
-        text.setText(packageName);
+        startActivityForResult(new Intent(android.provider.Settings.ACTION_SETTINGS), 0);
+        text.setText(getForegroundInfo());
+
+    }
+
+    private String getForegroundInfo() {
+        try {
+            TextView text = findViewById(R.id.txt_process);
+            ActivityManager am = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+            // The first in the list of RunningTasks is always the foreground task.
+            ActivityManager.RunningTaskInfo foregroundTaskInfo = am.getRunningTasks(1).get(0);
+            String foregroundTaskPackageName = foregroundTaskInfo.topActivity.getPackageName();
+            PackageManager pm = this.getPackageManager();
+            PackageInfo foregroundAppPackageInfo = null;
+
+            foregroundAppPackageInfo = pm.getPackageInfo(foregroundTaskPackageName, 0);
+
+            String foregroundTaskAppName = foregroundAppPackageInfo.applicationInfo.loadLabel(pm).toString();
+            return foregroundTaskAppName;
+        }catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        /*try {
+            boolean foregroud = false;
+            while(!foregroud){
+                foregroud = new ForegroundCheckTask().execute(getApplicationContext()).get();
+            }
+
+        }catch (ExecutionException | InterruptedException e){
+            e.printStackTrace();
+        }*/
+
+        return "";
+    }
+
+}
+
+class ForegroundCheckTask extends AsyncTask<Context, Void, Boolean> {
+
+    @Override
+    protected Boolean doInBackground(Context... params) {
+        final Context context = params[0].getApplicationContext();
+        return isAppOnForeground(context);
+    }
+
+    private boolean isAppOnForeground(Context context) {
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> appProcesses = activityManager.getRunningAppProcesses();
+        if (appProcesses == null) {
+            return false;
+        }
+        final String packageName = context.getPackageName();
+        for (ActivityManager.RunningAppProcessInfo appProcess : appProcesses) {
+            if (appProcess.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND && appProcess.processName.equals(packageName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
